f0aba785e0c8fc3a6993045522f714cbbb6f0071
Chris Cole <goosemanjack@gmail.com>
2014-09-17 15:22:23 -0700

    Video showing with VAST ad creatives inline.

--- video-test.js
+++ video-test.js
@@ -15,7 +15,21 @@
 	'http://media.w3.org/2010/05/bunny/movie.mp4'
 	];
 	
+	var vid;
+	var adTimes = [ 5.0, 15.0 ];
+	var playedAds = [false, false];
+	
+	var videoInfo = {
+		url: null,
+		currentTime: 0
+	};
+		
+	var pauseTime, intervalId, allDone = false;
+		
+	var vastDoc;
+	
 	var vastFileList = [
+	'test_vastAd.xml',
 	'ad3.liverail_com.xml',
 	'vast2VPAIDLinear.xml',
 	'doubleClick_xml.xml',
@@ -29,15 +43,87 @@
 	}
 	
 	function vastHere(vdoc){
-		console.log(vdoc);
-		
-		var c = vdoc.ads[0].creatives[0].getCompanionAds();
+		vastDoc = vdoc;
+		// console.log(vdoc);
+		// var c = vdoc.ads[0].creatives[0].getCompanionAds();
 	}
+	
+	function playHandler(e){
+		console.log('play');
+		if(vastDoc != null){
+			console.log('check vastDoc');
+		}
+		
+		intervalId = setInterval(timeUpdateHandler, 1000);
+	}
+	
+	
+	function seekToTime(e){
+		vid.removeEventListener('loadedmetadata', seekToTime);
+		vid.play();
+		vid.currentTime = videoInfo.currentTime;
+	}
+	
+	
+	function adEndedHandler(e){
+		vid.addEventListener('loadedmetadata', seekToTime);
+		vid.src = videoInfo.src + '#t=' + videoInfo.currentTime;
+		vid.removeEventListener('ended', adEndedHandler);
+	}
+	
+	function playAdNumber(num){
+		vid.pause();
+		var i;
+		var ad = vastDoc.ads[0];
+		var adSrc;
+		var cr = ad.creatives[num];
+		
+		playedAds[num] = true;
+		
+		if(cr.adType != 'linear'){
+			for(i=num+1; i < ad.creatives.length; i++){
+				cr = ad.creatives[i];
+				if(cr.adType == 'linear'){
+					break;
+				}
+			}
+		}
+		
+		if(cr && cr.linear){
+			videoInfo.currentTime = vid.currentTime;
+			adSrc = cr.linear.mediaFiles[0].url;
+			vid.src = adSrc;
+			vid.addEventListener('ended', adEndedHandler);
+			vid.play();
+		}
+	}
+
+	function timeUpdateHandler(e){
+		var i;
+		var time = vid.currentTime;
+		
+		if(vastDoc == null){
+			return;
+		}
+
+		for(i=0; i < adTimes.length; i++){
+			if(i == adTimes.length - 1 && playedAds[i] == true){
+				cancelInterval(intervalId);
+				allDone = true;
+			}
+			
+			if(time >= adTimes[i] && playedAds[i] == false){
+				playAdNumber(i);				
+			}
+		}
+	}
+
 	
 	function init(){
 		var btn = getEl('playBtn');
 		var i, sel, buf = [];
 		
+		vid = getEl('video1');
 		btn.addEventListener('click', vidtest.loadVideo);
 		
 		getEl('pauseBtn').addEventListener('click', vidtest.pauseVideo);
@@ -63,6 +149,10 @@
 		}
 		
 		sel.innerHTML = buf.join('');
+		
+		
+		vid.addEventListener('play', playHandler);
+		// vid.addEventListener('timeupdate', timeUpdateHandler);
 	}
 	
 	
@@ -76,7 +166,12 @@
 				vid.play();
 			}
 			else{			
-				vid.src = 'mov_bbb.mp4';
+				// vid.src = 'mov_bbb.mp4';
+				vid.src = videoList[0];
+				
+				videoInfo.src = vid.src;
+				
+				
 				setTimeout(function(){
 					vidtest.loaded = true;
 					vid.play();
